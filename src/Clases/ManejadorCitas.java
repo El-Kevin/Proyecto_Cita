de39d67e400eb15558cc829d97035fe1e679f989
HOME <HOME@DESKTOP-EH3MFAL>
2022-05-15 14:56:46 -0500

    implementar la clase manejador de citas el apartado de leer el archivo Json y escribir el archivo

--- src/Clases/ManejadorCitas.java
+++ src/Clases/ManejadorCitas.java
@@ -4,9 +4,13 @@
  */
 package Clases;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import javax.swing.filechooser.FileSystemView;
 import org.json.JSONArray;
@@ -41,14 +45,40 @@ public class ManejadorCitas {
          
          for (int i = 0; i < jsonList.length(); i++){
              JSONObject jsonObject = (JSONObject) jsonList.get(i);
-             CitaMedica userData = new CitaMedica();
-             userData.setDisponibilidad(jsonObject.getInt("Disponibilidad"));
+             CitaMedica userData = new CitaMedica() {};
              userData.setEspecialidad(jsonObject.getString("Especialidad"));
-             userData.setFechaCita(jsonObject.getString("Fecha Cita"));
              userData.setFechaEmision(jsonObject.getString("Fecha Emision"));
              userData.setNombremedico(jsonObject.getString("Nombre medico"));
+             userData.setFechaCita(jsonObject.getString("Fecha Cita"));
+              userData.setDisponibilidad(jsonObject.getInt("Disponibilidad"));
              dataList.add(userData);
          }
          return dataList;
+    }
+     
+     public void sobreescribirArchivo(ArrayList<CitaMedica> dataList) throws IOException{
+        File outputFile = new File(pathCita);
+        // Create the parent directory
+        outputFile.getParentFile().mkdir();
+        // Create the file into the parent directory
+        outputFile.createNewFile();
+        
+        JSONArray jsonList = new JSONArray();
+         for (CitaMedica user : dataList){
+             JSONObject jsonObject = new JSONObject();
+             jsonObject.put("Especialidad", user.getEspecialidad());
+             jsonObject.put("Fecha Emision", user.getFechaEmision());
+             jsonObject.put("Nombre medico", user.getNombremedico());
+             jsonObject.put("Fecha Cita", user.getFechaCita());
+             jsonObject.put("Disponibilidad", user.getDisponibilidad());
+             jsonList.put(jsonObject);
+         }
+         // Create the buffer to write
+         BufferedWriter bufferWriter = Files.newBufferedWriter(
+         Paths.get(outputFile.toURI()));
+         
+         jsonList.write(bufferWriter);
+         bufferWriter.close();
+          
     }
 }
