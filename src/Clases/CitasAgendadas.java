10c732fd025d59353f0c6ed415d504114653ba93
luischiliguano <95940533+luischiliguano@users.noreply.github.com>
2022-05-14 19:37:19 -0500

    Creando Clases

---
+++ src/Clases/CitasAgendadas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User
+ */
+public class CitasAgendadas extends CitaMedica {
+    private int numeroCitasAgendadas;
+    private ArrayList <CitaMedica> citasAgendadas;
+
+    @Override
+    public int calcularDiasFaltantes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public CitasAgendadas() {
+    }
+    
+    public void consultarCitasAgendadas(){
+        
+    }
+    
+    public void visualizarCitasAgendadas(){
+        
+    }
+}
